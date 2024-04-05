5ca9c1d6ae090d20ff7b5ab679588b8a33371413
Jacques HU <jacques33185@gmail.com>
2024-04-05 14:56:46 +0200

    - made status bar transparent

--- app/src/main/java/com/example/seriously/MainActivity.kt
+++ app/src/main/java/com/example/seriously/MainActivity.kt
@@ -9,22 +9,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.seriously.ui.theme.SeriouslyTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SeriouslyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val systemUiController = rememberSystemUiController()
+            systemUiController.setSystemBarsColor(color = Color.Transparent)
+
+            Text("Hello world!")
         }
     }
 }
