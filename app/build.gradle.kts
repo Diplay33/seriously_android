d63dd1119ceee810180dc07ecf1b9a804ae2b972
Jacques HU <jacques33185@gmail.com>
2024-04-25 10:58:06 +0200

    - added navigation compose lib to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.google.accompanist)
+    implementation(libs.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
